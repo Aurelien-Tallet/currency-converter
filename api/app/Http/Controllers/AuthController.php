c5b28cb95b9b5acca9b5cd60618540e437e92297
Aurelien-Tallet <aurelien.jarte@gmail.com>
2022-08-27 15:06:04 +0200

    admin: private route added

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -5,16 +5,33 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
     public function authenticate(Request $request)
     {
-        $user = User::where('email', $request->email)->first();
-        if(Hash::check($request->password, $user->password)) {
-            return response()->json([
-                'token' => $user->createToken(time())->plainTextToken,
-                'message' => 'success'], 200);
-        }
+        $request->validate([
+			'email' => 'required|email',
+			'password' => 'required',
+		]);
+
+		$user = User::where('email', $request->email)->first();
+
+		if (! $user || ! Hash::check($request->password, $user->password)) {
+			throw ValidationException::withMessages([
+				'email' => ['The provided credentials are incorrect.'],
+			]);
+		}
+
+		return response()->json([
+			'user' => $user,
+			'access_token' => $user->createToken($request->email)->plainTextToken
+		], 200);
     }
+    public function logout(Request $request) {
+
+		$request->user()->currentAccessToken()->delete();
+		return response()->json(null, 200);
+	}
 }
